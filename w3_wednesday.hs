4bef0908dd388097ccfad98fd0c934fec9e362be
Samantha Woravka <sworavka@proton.me>
2026-09-16 16:31:32 -0500

    w3_wednesday

---
+++ w3_wednesday.hs
@@ -0,0 +1,15 @@
+-- ranges
+numbers :: [Int]
+numbers = [1 .. 10]
+
+evens :: [Int]
+evens = [2, 4 .. 20]
+
+countByFives :: [Int]
+countByFives = [5, 10 .. 50]
+
+naturals :: [Int]
+naturals = [1 ..]
+
+evenStream :: [Int]
+evenStream = [2, 4 ..]
